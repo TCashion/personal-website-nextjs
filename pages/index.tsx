ab2d3121e62cd3e0ca389ba51285dae86d6a3fc7
TCashion <58757351+TCashion@users.noreply.github.com>
2021-12-15 09:44:25 -0700

    first image replace on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 import Card from '../components/Card';
 import InvisiCard from '../components/InvisiCard';
 import Button from '../components/Button';
+import BannerImage from '../public/images/home/banner_cropped.png';
 
 const images = [
-  '/images/home/banner_cropped.png',
   '/images/home/trekking.jpg',
   '/images/home/thankyoubackground.jpg',
 ];
@@ -19,15 +20,9 @@ const imageStyle = {
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div
-        style={{
-          background: `url(${images[0]})`,
-          height: '650px',
-          backgroundPositionY: 'bottom 85%',
-          ...imageStyle,
-        }}
-      >
-        <div className="w-full sm:w-1/2 h-full p-8 flex flex-col">
+      <div className="flex items-center">
+        <Image src={BannerImage} height={1650} priority />
+        <div className="w-full sm:w-1/2 h-full p-8 flex flex-col absolute">
           <div className="my-auto">
             <InvisiCard>
               <h2>Portfolio</h2>
@@ -53,9 +48,9 @@ const Home: NextPage = () => {
             <Card extraClasses="shadow-light">
               <h2>Blog</h2>
               <p>
-                I love to write about things that excite me, adventures I&apos;ve
-                had, and some of my most poignant memories. In my blog, I share
-                thoughts and stories that are meaningful to me.
+                I love to write about things that excite me, adventures
+                I&apos;ve had, and some of my most poignant memories. In my
+                blog, I share thoughts and stories that are meaningful to me.
               </p>
               <Link href="/posts">
                 <a>
@@ -90,6 +85,6 @@ const Home: NextPage = () => {
       </div>
     </Layout>
   );
-}
+};
 
-export default Home
+export default Home;
