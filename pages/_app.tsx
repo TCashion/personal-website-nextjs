e64765e20771b51921d4866aa6ccd89420eaaa97
Travis Cashion <cashion.travisg@gmail.com>
2022-01-10 09:03:39 -0700

    feat: wake up projects once at initial render (#12)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
+import { herokuProjectsUrls } from '../lib/projects/projectsData';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  const [wakeUpCalled, setWakeUpCalled] = useState(false);
+
+  useEffect(() => {
+    if (!wakeUpCalled) {
+      console.log(herokuProjectsUrls);
+      herokuProjectsUrls.forEach((projectUrl) => {
+        if (projectUrl) {
+          fetch(projectUrl, { mode: 'no-cors' }).then((res) =>
+            console.log(`Woke up ${projectUrl}`)
+          );
+        }
+      });
+      setWakeUpCalled(true);
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default App;
