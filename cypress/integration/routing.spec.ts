eb563aa0c17cfed5ff155dcc11a89e109173b2ff
TCashion <58757351+TCashion@users.noreply.github.com>
2022-02-03 08:11:44 -0700

    add tests for sidenav

--- cypress/integration/routing.spec.ts
+++ cypress/integration/routing.spec.ts
@@ -1,20 +1,46 @@
 /// <reference types="cypress" />
 
 describe('routing', () => {
-  context('the main nav bar', () => {
+  describe('the main nav bar', () => {
     it('routes appropriately', () => {
       cy.visit(Cypress.env('localServerURL'));
 
       cy.get('[data-test-id="main-nav-portfolio"]').click();
       cy.url().should('include', '/projects');
-      
+
       cy.get('[data-test-id="main-nav-blog"]').click();
       cy.url().should('include', '/posts');
-      
+
       cy.get('[data-test-id="main-nav-about"]').click();
       cy.url().should('include', '/about');
-      
+
       cy.get('[data-test-id="main-nav-home"]').click();
+      cy.url().should('equal', Cypress.env('localServerURL'));
+    });
+  });
+
+  describe('the side nav bar', () => {
+    beforeEach(() => {
+      cy.viewport(360, 640);
+    });
+
+    it('routes appropriately', () => {
+      cy.visit(Cypress.env('localServerURL'));
+
+      cy.get('[data-test-id="hamburger-menu"]').click();
+      cy.get('[data-test-id="side-nav-portfolio"]').click();
+      cy.url().should('include', '/projects');
+      
+      cy.get('[data-test-id="hamburger-menu"]').click();
+      cy.get('[data-test-id="side-nav-blog"]').click();
+      cy.url().should('include', '/posts');
+      
+      cy.get('[data-test-id="hamburger-menu"]').click();
+      cy.get('[data-test-id="side-nav-about"]').click();
+      cy.url().should('include', '/about');
+      
+      cy.get('[data-test-id="hamburger-menu"]').click();
+      cy.get('[data-test-id="side-nav-home"]').click();
       cy.url().should('equal', Cypress.env('localServerURL'));
     });
   });
