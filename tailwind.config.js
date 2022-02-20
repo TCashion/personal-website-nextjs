79d7ce8285d429b6060be01d74b154d89a38e2de
TCashion <58757351+TCashion@users.noreply.github.com>
2022-02-19 18:07:43 -0700

    turn on purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     purgeLayersByDefault: true,
   },
   purge: {
-    // enabled: true,
+    enabled: true,
     content: [
       './components/**/*.{js,ts,jsx,tsx}',
       './pages/**/*.{js,ts,jsx,tsx}',
